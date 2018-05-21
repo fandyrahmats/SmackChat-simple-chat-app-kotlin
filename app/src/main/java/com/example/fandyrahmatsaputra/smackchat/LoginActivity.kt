8b5dead118928d9f331d4071e3b79a54632b78a3
fandyrahmats <fanidayam@gmail.com>
2018-05-21 11:05:46 +0800

    add login component

---
+++ app/src/main/java/com/example/fandyrahmatsaputra/smackchat/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.example.fandyrahmatsaputra.smackchat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun signUpClicked(view:View) {
+
+    }
+
+    fun loginClicked(view: View) {
+
+    }
+
+}
